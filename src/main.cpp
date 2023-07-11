f35915e6c60631d8ce11950a327ec6ae60294c44
Valkyrie <60992540+acaValkyrie@users.noreply.github.com>
2023-07-11 16:36:15 +0900

    made PrintTm()

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 
 
 const char* ntpServer = "ntp.nict.jp";
+// JST: 9hours late. 9h = 32400s
 const long  gmtOffset_sec = 32400;
 const int   daylightOffset_sec = 0;
 
@@ -14,8 +15,12 @@ void WifiSyncJST()
   // WiFi接続
   WiFi.begin(ssid, password);
   Serial.println("Connecting to WiFi");
+  int count = 0;
   while (WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
+    if(count % 10 == 0){
+      Serial.print(".");
+    }
+    count++;
   }
   Serial.println("Connected");
   // NTPサーバからJST取得
@@ -41,14 +46,20 @@ void setup()
   WifiSyncJST();
 }
 
+void PrintTm(struct tm);
+
 void loop()
 {
   time_t now = time(NULL);
   tm = localtime(&now);
+  PrintTm(tm);
+}
+
+void PrintTm(struct tm){
   int month, day, hour, minute, second;
   month = tm->tm_mon + 1;
   day = tm->tm_mday;
-  hour = tm->tm_hour;
+  hour = tm->tmHour;
   minute = tm->tm_min;
   second = tm->tm_sec;
   Serial.printf("%d/%d %d:%d:%d\n", month, day, hour, minute, second);
